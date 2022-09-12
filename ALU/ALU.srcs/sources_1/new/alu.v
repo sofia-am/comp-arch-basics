33080d486ba373405e259c94fc31dc00eda2f4ab
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-09-12 20:10:13 -0300

    Se agrega signo para la resta

--- ALU/ALU.srcs/sources_1/new/alu.v
+++ ALU/ALU.srcs/sources_1/new/alu.v
@@ -10,7 +10,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module alu
-    #(  parameter number_bus_input = 5,
+    #(  parameter number_bus_input = 4,
         parameter number_bus_output = 5,
         parameter number_bus_operation = 6,
         
@@ -23,11 +23,12 @@ module alu
         parameter SRL = 6'b000010,
         parameter NOR = 6'b100111
      )
-    (   input wire signed [number_bus_input-1:0] data_a, [number_bus_input-1:0] data_b,
+    (   input wire [number_bus_input-1:0] data_a, [number_bus_input-1:0] data_b,
         input wire [number_bus_operation-1:0] operation,
-        output reg signed [number_bus_output-1:0] result,
-        output reg signed carry,
-        output reg signed zero
+        output reg [number_bus_output:0] result,
+        output reg carry,
+        output reg zero,
+        output reg signo
     );
     
 reg [number_bus_output:0] result_aux;
@@ -35,52 +36,70 @@ reg [number_bus_output:0] result_aux;
 always @(data_a or data_b or operation) begin
     case(operation)
         ADD: begin
-            result_aux = data_a + data_b;
-            result = result_aux[number_bus_output-1:0];
-            carry = result_aux[number_bus_output];
-            zero = (result_aux == 5'b0);
-            end
-        SUB: begin
             carry = 0;
             zero = 0;
-            result_aux = data_a - data_b;
-            result = result_aux[number_bus_output-1:0];
-            zero = (result_aux == 5'b0);
+            signo = 0;
+            result = data_a + data_b;
+            carry = (result == 5'b10000);
+            zero = (result == 5'b0);
+            result = result[number_bus_output-1:0];
             end
-        AND: begin
-            carry = 0;
-            zero = 0;
-            result = data_a & data_b;
-            end
-        OR: begin
-            carry = 0;
-            zero = 0;
-            result = data_a | data_b;
-            end
-        XOR: begin
-            carry = 0;
-            zero = 0;
-            result = data_a ^ data_b;
-            end
-        SRA: begin
-            carry = 0;
-            zero = 0;
-            result = data_a >>> data_b;
-            end
-        SRL: begin
-            carry = 0;
-            zero = 0;
-            result = data_a << data_b;
-            end
-        NOR: begin
-            carry = 0;
-            zero = 0;
-            result =~(data_a | data_b);
-            end
+//        SUB: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            if(data_b > data_a) begin
+//               signo = 1;
+//               result = data_b - data_a;
+//            end
+//            else if(data_b < data_a) begin
+//                signo = 0;
+//                result = data_a - data_b;
+//            end
+//            zero = (result == 5'b0);
+//            result = result[number_bus_output-1:0];
+//            end
+//        AND: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            result = data_a & data_b;
+//            end
+//        OR: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            result = data_a | data_b;
+//            end
+//        XOR: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            result = data_a ^ data_b;
+//            end
+//        SRA: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            result = data_a >>> data_b;
+//            end
+//        SRL: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            result = data_a << data_b;
+//            end
+//        NOR: begin
+//            carry = 0;
+//            zero = 0;
+//            signo = 0;
+//            result =~(data_a | data_b);
+//            end
         default: begin
             result = 0;
             carry = 0;
             zero = 0;
+            signo = 0;
             end
     endcase
 end

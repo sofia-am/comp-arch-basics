5447fde1775a4794dda7791ad4f61a12fd949c80
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-09-12 20:09:48 -0300

    Se agrega signo para la resta

--- ALU/ALU.srcs/sim_1/new/alu_tb.v
+++ ALU/ALU.srcs/sim_1/new/alu_tb.v
@@ -1,27 +1,17 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.09.2022 09:07:43
-// Design Name: 
-// Module Name: alu_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
+// Module Name: alu
+// Project Name: TP1 ALU - Arquitectura de Computadoras
+// Authors: Amallo, Sofia - Raya, Matias
+// Target Devices: Basys2/3
 // Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
+// Implementaci�n de una ALU b�sica con un testbench
 // Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module alu_tb( );
-parameter number_bus_input = 5;
+parameter number_bus_input = 4;
 parameter number_bus_output = 5;
 parameter number_bus_operation = 6;
 
@@ -30,14 +20,23 @@ reg [number_bus_operation-1:0] operation;
 wire [number_bus_output-1:0] result;
 wire carry;
 wire zero;
+wire signo;
 
 initial
 begin
+    data_a=1;
+    data_b=8'b1111;
+    operation=32;
+    #10
     data_a=5;
-    data_b=8'b11111111;
+    data_b=8'b0011;
     operation=32;
     #10
     data_a=3;
+    data_b=8;
+    operation=34;
+    #10
+    data_a=10;
     data_b=4;
     operation=34;
     #10
@@ -74,6 +73,7 @@ alu alu0(
     .operation(operation),
     .result(result),
     .carry(carry),
-    .zero(zero)
+    .zero(zero),
+    .signo(signo)
 );
 endmodule

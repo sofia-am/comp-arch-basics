bae0a5f49fbcf85410643e217d0c908eb150b490
sofia-am <sofiamallo07@hotmail.com>
2022-09-14 17:41:41 -0300

    descartamos uso de otro modulo, se modifican los inputs a la ALU

--- ALU/ALU.srcs/sources_1/new/alu.v
+++ ALU/ALU.srcs/sources_1/new/alu.v
@@ -8,8 +8,8 @@
 // Implementaciï¿½n de una ALU bï¿½sica con un testbench
 // Revision 0.01 - File Created
 //////////////////////////////////////////////////////////////////////////////////
-
-module switches
+/*
+module inputs
     #(  parameter DATA_SIZE = 4,
         parameter OPCODE_SIZE = 6,
         parameter SWITCHES = 6
@@ -42,38 +42,65 @@ end
     assign o_dato_a = dato_a;
     assign o_dato_b = dato_b;
     assign o_opcode = opcode;    
-endmodule
+endmodule*/
 
 module alu
     #(  parameter INPUT_SIZE = 4,
         parameter OUTPUT_SIZE = 5,
-        parameter OPCODE_SIZE = 6,
-        
-        parameter ADD = 6'b100000,
-        parameter SUB = 6'b100010,
-        parameter AND = 6'b100100,
-        parameter OR  = 6'b100101,
-        parameter XOR = 6'b100110,
-        parameter SRA = 6'b000011,
-        parameter SRL = 6'b000010,
-        parameter NOR = 6'b100111
+        parameter OPCODE_SIZE = 6    
      )
-    (   input wire [INPUT_SIZE-1:0] data_a, [INPUT_SIZE-1:0] data_b,
-        input wire [OPCODE_SIZE-1:0] operation,
+    (   //input wire [INPUT_SIZE-1:0] data_a, [INPUT_SIZE-1:0] data_b,
+        //input wire [OPCODE_SIZE-1:0] operation,
+        input wire [OPCODE_SIZE-1:0] numeros,
+        input wire load_1, //dato a
+        input wire load_2, //dato b
+        input wire load_3, //opcode
         input wire clock,
         output wire [OUTPUT_SIZE-1:0] o_result,
         output wire o_carry,
         output wire o_zero,
         output wire o_signo
     );
+/*
+    inputs entradas(
+        .clock(clock),
+        .o_dato_a(data_a),
+        .o_dato_b(data_b),
+        .o_opcode(operation)
+    );*/
 
-reg [OUTPUT_SIZE-1:0] result;
+//parámetros para facilitar la comprensión de los switch cases
+localparam ADD = 6'b100000;
+localparam SUB = 6'b100010;
+localparam AND = 6'b100100;
+localparam OR  = 6'b100101;
+localparam XOR = 6'b100110;
+localparam SRA = 6'b000011;
+localparam SRL = 6'b000010;
+localparam NOR = 6'b100111;
+
+reg [OPCODE_SIZE-1:0] opcode = {OPCODE_SIZE{1'b0}};
+reg [INPUT_SIZE-1:0] data_a = {INPUT_SIZE{1'b0}};
+reg [INPUT_SIZE-1:0] data_b = {INPUT_SIZE{1'b0}};
+reg [OUTPUT_SIZE-1:0] result = {OUTPUT_SIZE{1'b0}};
 reg carry; //si no se define nada, es de 1 bit
 reg zero;
 reg signo;
 
 always @(posedge clock) begin
-    case(operation)
+    if(load_1) begin
+        data_a = numeros;
+    end
+    else if(load_2) begin
+        data_b = numeros;
+    end
+    else if(load_3) begin
+        opcode = numeros;
+    end
+end
+
+always @(posedge clock) begin
+    case(opcode)
         ADD: begin
             carry = 1'b0;
             zero = 1'b0;

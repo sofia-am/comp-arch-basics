2ba41e11a836e3f6e412ca10d95aceed4055278a
sofia-am <sofiamallo07@hotmail.com>
2022-09-20 18:15:51 -0300

    subo todo porque puedo

---
+++ ALU/ALU.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+//Date        : Thu Sep 15 07:32:04 2022
+//Host        : DESKTOP-K2VPBA5 running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   ();
+
+
+  design_1 design_1_i
+       ();
+endmodule
---
+++ ALU/ALU.gen/sources_1/bd/design_1/sim/design_1.v
@@ -0,0 +1,17 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+//Date        : Thu Sep 15 07:32:04 2022
+//Host        : DESKTOP-K2VPBA5 running 64-bit major release  (build 9200)
+//Command     : generate_target design_1.bd
+//Design      : design_1
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=0,numReposBlks=0,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
+module design_1
+   ();
+
+
+endmodule
